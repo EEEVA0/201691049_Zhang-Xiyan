ddbb6618891370849fce8cc4975a91bd3158470c
EEEVA0 <1719123801@qq.com>
2026-04-16 22:55:49 +0800

    Add description

--- pages/models.py
+++ pages/models.py
@@ -4,6 +4,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_year = models.IntegerField()
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
