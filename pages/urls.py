48ea814bda5a3780585634086d74e6f8f2f81187
EEEVA0 <1719123801@qq.com>
2026-04-16 22:38:37 +0800

    Set up Django project with Book model and basic /books/ API

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import homePageView
+from .views import book_list
 
 urlpatterns = [
-    path("", homePageView, name="home"),
+    path('books/', book_list, name='book-list'),
 ]
